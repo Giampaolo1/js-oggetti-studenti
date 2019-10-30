2c18638378d21b7333d90872edb71c2e126e7a1d
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-30 13:46:07 +0100

    Scaf

---
+++ script/main.js
@@ -0,0 +1,8 @@
+//  - Creare un OGGETTO che descriva uno studente con le seguenti proprietà:
+// NOME COGNOME ETA, stampare aschermo attraverso il FOR in tutte le proprietà
+
+//  - Creare un ARRAY di OGGETTI di studenti.
+// CICLARE su tutti gli studenti e STAMPARE per ognuno nome e cognome
+
+//  - Dare la possibilità all utente attraverso 3 PROMPT di aggiungere un nuovo
+// OGGETTO STUDENTE inserendo nell ordine nome,cognome,età
