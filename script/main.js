841f1f30eed2fcff13efa9a90f52a3688e3cbb91
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-30 17:14:59 +0100

    studenti

--- script/main.js
+++ script/main.js
@@ -1,8 +1,39 @@
 //  - Creare un OGGETTO che descriva uno studente con le seguenti proprietà:
-// NOME COGNOME ETA, stampare aschermo attraverso il FOR in tutte le proprietà
+// NOME COGNOME ETA. STAMPARE a schermo attraverso il FOR in tutte le proprietà.
 
 //  - Creare un ARRAY di OGGETTI di studenti.
-// CICLARE su tutti gli studenti e STAMPARE per ognuno nome e cognome
-
+// CICLARE su tutti gli studenti e STAMPARE per ognuno nome e cognome,
 //  - Dare la possibilità all utente attraverso 3 PROMPT di aggiungere un nuovo
-// OGGETTO STUDENTE inserendo nell ordine nome,cognome,età
+// OGGETTO STUDENTE inserendo nell ordine nome,cognome,età.
+
+//  - Creare un OGGETTO che descriva uno studente con le seguenti proprietà:
+
+var studente = {
+  "Nome": "Fabio",
+  "Cognome": "Rossi",
+  "Età": "30"
+};
+
+// console.log(studente);
+
+//  - Creare un ARRAY di OGGETTI di studenti.
+var studenti = [stud1,stud2,stud3];
+
+console.log(studenti);
+
+// lista studenti
+var stud1 = {
+  "Nome": "Fabio",
+  "Cognome": "Rossi",
+  "Età": "30"
+};
+var stud2 = {
+  "Nome": "Marco",
+  "Cognome": "Bianchi",
+  "Età": "31"
+};
+var stud3 = {
+  "Nome": "Simone",
+  "Cognome": "Verdi",
+  "Età": "32"
+};
